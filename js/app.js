bb232318f81bca6c295348c02dd9bbcb1594909a
lukep <luke.pacholski@gmail.com>
2020-11-27 19:29:19 -0600

    blah

--- js/app.js
+++ js/app.js
@@ -57,16 +57,6 @@ window.addEventListener("resize", () => {
 
 
 
-let acl = new Accelerometer({frequency: 60});
-
-acl.addEventListener('reading', () => {
-  console.log("Acceleration along the X-axis " + acl.x);
-  console.log("Acceleration along the Y-axis " + acl.y);
-  console.log("Acceleration along the Z-axis " + acl.z);
-});
-
-acl.start();
-
 
 
 function handleOrientation(event) {
@@ -75,88 +65,14 @@ function handleOrientation(event) {
   console.log(x,y);
   root.style.setProperty("--rotate-x", -x + "deg");
   root.style.setProperty("--rotate-y", y + "deg");
+  document.querySelector(".log").innerText = x + "," + y;
  
 }
 
-window.addEventListener('deviceorientation', handleOrientation);
 
 
-function handlePermission() {
+// console.log(window.DeviceOrientationEvent);
 
-navigator.permissions.query({name:'geolocation'}).then(function(result) {
-    if (result.state == 'granted') {
-      report(result.state);
-      
-    } else if (result.state == 'prompt') {
-      report(result.state);
-      
-      
-    } else if (result.state == 'denied') {
-      report(result.state);
-      
-    }
-    result.onchange = function() {
-      report(result.state);
-    }
-  });
-}
-
-
-  function report(state) {
-  console.log('Permission ' + state);
-}
-
-// handlePermission();
-
-
-
-
-const permissionsToRequest = {
-  permissions: ["bookmarks", "history"],
-  origins: ["https://developer.mozilla.org/"]
-}
-
-function requestPermissions() {
-
-  function onResponse(response) {
-    if (response) {
-      console.log("Permission was granted");
-    } else {
-      console.log("Permission was refused");
-    }
-    return browser.permissions.getAll();  
-  }
-
-  browser.permissions.request(permissionsToRequest)
-    .then(onResponse)
-    .then((currentPermissions) => {
-    console.log(`Current permissions:`, currentPermissions);
-  });
-}
-
-// document.querySelector("body").addEventListener("click", requestPermissions);
-
-
-
-function permission () {
-    if ( typeof( DeviceMotionEvent ) !== "undefined" && typeof( DeviceMotionEvent.requestPermission ) === "function" ) {
-        // (optional) Do something before API request prompt.
-        DeviceMotionEvent.requestPermission()
-            .then( response => {
-            // (optional) Do something after API prompt dismissed.
-            if ( response == "granted" ) {
-                window.addEventListener( "devicemotion", (e) => {
-                    // do something for 'e' here.
-                })
-            }
-        })
-            .catch( console.error )
-    } else {
-        alert( "DeviceMotionEvent is not defined" );
-    }
-}
-const btn = document.querySelector( "body" );
-// btn.addEventListener( "click", permission );
-btn.addEventListener( "touchstart", permission );
-
-
+//     if (window.DeviceOrientationEvent) {
+//         window.addEventListener('deviceorientation', handleOrientation);
+//       }
